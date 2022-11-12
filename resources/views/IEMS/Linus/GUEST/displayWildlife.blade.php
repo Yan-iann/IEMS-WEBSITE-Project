e326f5bac796a7f13afef5a06de7ffb615638fa7
Jibrael Yap <jibraelpyap@su.edu.ph>
2022-11-12 23:37:17 +0800

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is behind 'origin/main' by 2 commits, and can be fast-forwarded.
       (use "git pull" to update your local branch)
    
     Changes to be committed:
    	modified:   resources/views/IEMS/Linus/GUEST/GuestWLDashboard.blade.php
    	modified:   resources/views/IEMS/Linus/GUEST/displayWildlife.blade.php

--- resources/views/IEMS/Linus/GUEST/displayWildlife.blade.php
+++ resources/views/IEMS/Linus/GUEST/displayWildlife.blade.php
@@ -1,4 +1,5 @@
 
+
 <div class="modal fade m-3" id="ModalWildlife{{$item->info_ID}}" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg">
     <div class="modal-content  bg-light">
@@ -7,10 +8,10 @@
         <h5 class="modal-title  text-center">Wildlife Details</h5>
         <button type="button" class="btn-close bg-info" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      
+
       <div class="modal-body ">
           <div class="row g-4 m-4 p-0 d-flex align-items-stretch g-l ">
-            
+
             <div class="col-12 col-lg-4 d-flex justify-content-center">
             <img class="imageWildlife" src="{{ asset($item->wildlife_pic) }}" alt="Card image cap">
             </div>
@@ -23,7 +24,7 @@
               <h3 class="detailsView">{{ $item->wildlife_name}}</h5 class="detailsView">
               </div>
               </div>
-            
+
 
             <div class="row">
               <div class="col-12 col-lg-12"><br>
@@ -67,21 +68,21 @@
               <label for="formGroupExampleInput2" class="form-label">Location</label>
               <h5 class="location">{{ $item->wildlife_location}}</h5 class="detailsView">
             </div>
-            
+
             <div class="col-12 col-lg-12">
               <label for="exampleFormControlTextarea1" class="form-label">Description</label>
               <p class="detailsView">{{ $item->wildlife_desc}}</p class="detailsView">
-                
+
             </div>
 
 
           </div>
-        
+
       </div>
       <div class="modal-footer border-0">
 
       </div>
     </div>
-    
+
   </div>
 </div>
