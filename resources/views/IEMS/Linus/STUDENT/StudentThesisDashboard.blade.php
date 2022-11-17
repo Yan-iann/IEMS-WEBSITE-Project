3a18e7981331ae94d03436edb38bb85246ef7dbd
Jibrael Yap <jibraelpyap@su.edu.ph>
2022-11-18 04:22:32 +0800

    added Email IEMS modal for journal and display for student and Faculty users
     Changes to be committed:
    	new file:   public/Logo_IEMS.ico
    	modified:   public/favicon.ico
    	modified:   resources/views/IEMS/Linus/GUEST/displayJournal.blade.php
    	modified:   resources/views/IEMS/Linus/GUEST/displayThesis.blade.php
    	modified:   resources/views/IEMS/Linus/STUDENT/StudentThesisDashboard.blade.php
    	modified:   resources/views/IEMS/Linus/STUDENT/displayJournal.blade.php
    	modified:   resources/views/IEMS/Linus/STUDENT/displayThesis.blade.php

--- resources/views/IEMS/Linus/STUDENT/StudentThesisDashboard.blade.php
+++ resources/views/IEMS/Linus/STUDENT/StudentThesisDashboard.blade.php
@@ -38,22 +38,30 @@
 
         {{-- Information Cards --}}
         <section class="col-12">
-    @foreach($thesis as $item)
-        <div class="col-6 col-md-4 col-lg-3 m-4" data-bs-toggle="modal" data-bs-target="#ModalThesis{{$item->info_ID}}">
-            <div class="card border-dark" style="width: 18rem;">
-                  <div class="card-body bg-light ">
-                    <p class="text-muted fst-italic">{{$item->date_published }}</p>
-                    <h4 class="card-title">{{$item->thesis_title}}</h4>
-                    <br>
-                    <p class="card-subtitle mb-2 text-muted">{{$item->thesis_author}}</p>
-                  </div>
-                <div class="card-footer border-0"></div>
+            <div class="container-fluid">
+              <div class="row g-5 m-4 p-0 d-flex align-items-stretch g-l">
 
+            @foreach($thesis as $item)
+                <div class="col-6 col-md-4 col-lg-3 d-flex align-items-stretch" data-bs-toggle="modal" data-bs-target="#ModalThesis{{$item->info_ID}}">
+                    <div class="card border-dark" style="width: 18rem;">
+                          <div class="card-body bg-light ">
+                            <p class="text-muted fst-italic">{{$item->date_published }}</p>
+                            <h4 class="card-title">{{$item->thesis_title}}</h4>
+                            <br>
+                            <p class="card-subtitle mb-2 text-muted">{{$item->thesis_author}}</p>
+                          </div>
+                        <div class="card-footer border-0"></div>
+
+                    </div>
+                </div>
+                @include('IEMS.Linus.STUDENT.displayThesis')
+            @endforeach
+
+                </div>
             </div>
-        </div>
-        @include('IEMS.Linus.STUDENT.displayThesis')
-    @endforeach
-        </section>
+                </section>
+
+
 
 <!--advance search-->
 <form action="{{ route('advanceSearchThesis') }}" method="GET" enctype="multipart/form-data">
--- resources/views/IEMS/Linus/STUDENT/displayJournal.blade.php
+++ resources/views/IEMS/Linus/STUDENT/displayJournal.blade.php
@@ -1,3 +1,4 @@
+{{-- Jounral Article Information Card --}}
 <div class="modal fade" id="ModalJournal{{$item->info_ID}}" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg">
     <div class="modal-content bg-light">
@@ -6,31 +7,31 @@
         <h5 class="modal-title  text-center">Journal Article Details</h5>
         <button type="button" class="btn-close btn-info bg-info" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      
+
       <div class="modal-body">
         <div class="container-fluid">
           <div class="row g-4 m-4 p-0 d-flex align-items-stretch g-l">
 
-   
 
-           
+
+
             <div class="col-12">
               <label for="formGroupExampleInput" class="form-label">Journal Title</label>
               <h2 class="detailsView">{{ $item->journal_title}}</h4 >
             </div>
-            
+
 
               <div class="col-12">
                 <label for="formGroupExampleInput" class="form-label">Author/s</label>
                 <p>{{ $item->journal_author}}</p>
               </div>
-           
+
 
             <div class="col-12">
               <label for="formGroupExampleInput2" class="form-label">Journal Reference</label>
               <p>{{ $item->journal_reference}}</p>
             </div>
-            
+
             <div class="col-12">
               <!-- filler  -->
             </div>
@@ -44,9 +45,36 @@
         </div>
       </div>
       <div class="modal-footer border-0">
-       
+        <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#Notify{{$item->info_ID}}">View</button>
       </div>
     </div>
-  
+
   </div>
 </div>
+
+
+
+
+{{-- Notify Modal --}}
+<div class="modal fade" id="Notify{{$item->info_ID}}" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable ">
+      <div class="modal-content bg-light">
+
+        <div class="modal-header border-0 text-center">
+          <h5 class="modal-title  text-center"></h5>
+          <button type="button" class="btn-close btn-info bg-info" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+
+        <div class="modal-body border-0">
+          <div class="container-fluid">
+            <div class="row g-4 m-4 p-0 d-flex align-items-stretch g-l">
+              <p> To view this document, please email us at <a href="mailto:iems@su.edu.ph">iems@su.edu.ph </a> regarding on viewing this document</p>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer border-0">
+          <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#ModalJournal{{$item->info_ID}}">Okay</button>
+        </div>
+      </div>
+    </div>
+  </div>
