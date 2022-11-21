d53be7bb116e490a4a0de8962c59e845c5ab19ed
Jibrael Yap <jibraelpyap@su.edu.ph>
2022-11-22 00:31:52 +0800

    Fixed Admin Layouts, added searchbar fixed the di ma scroll padung lalom
     Changes to be committed:
    	modified:   resources/views/IEMS/Linus/STUDENT/StudentWLDashboard.blade.php
    
     Changes not staged for commit:
    	modified:   public/js/locomotive.js
    	modified:   public/js/script.js
    	modified:   resources/views/IEMS/Linus/ADMIN/adminDashboard.blade.php
    	modified:   resources/views/IEMS/Linus/ADMIN/displayAdminAcc.blade.php
    	modified:   resources/views/IEMS/Linus/ADMIN/displayFacultyAcc.blade.php
    	modified:   resources/views/IEMS/Linus/ADMIN/displayStudentAcc.blade.php
    	modified:   resources/views/layouts/IEMS_Layout.blade.php

--- resources/views/IEMS/Linus/STUDENT/StudentWLDashboard.blade.php
+++ resources/views/IEMS/Linus/STUDENT/StudentWLDashboard.blade.php
@@ -1,134 +1,139 @@
 @extends('layouts.S_Layout')
 @section('content')
     <div class="container-fluid">
-                                @if(Session::get('fail'))
-                                <div class="alert alert-danger">
-                                    {{ Session::get('fail')}}
-                                </div>
-                                @endif
+        @if (Session::get('fail'))
+            <div class="alert alert-danger">
+                {{ Session::get('fail') }}
+            </div>
+        @endif
 
-                                @if(Session::get('sucess'))
-                                    <div class="alert alert-success">
-                                      {{ Session::get('sucess')}}
-                                    </div>
-                                @endif
+        @if (Session::get('sucess'))
+            <div class="alert alert-success">
+                {{ Session::get('sucess') }}
+            </div>
+        @endif
         {{-- Page Name, and Burger Icon. AND Search Bar --}}
         <div class="row d-flex justify-content-between">
             <div class="col-12 col-md-6 col-lg-6 order-sm-2 order-md-1">
-                 <div class="home-content">
+                <div class="home-content">
                     <div class="col-12 col-md-6 col-lg-6 d-flex align-items-center">
                         <i class='bx bx-menu'></i>
                         <span class="text">Critters</span>
                     </div>
-                 </div>
+                </div>
             </div>
 
-                    <div class="col-12 col-md-6 col-lg-6 order-sm-1 order-md-2">
-                        <form style="text-align: center;"class="form-inline my-2 my-lg=0" type="get" action="{{ route('S_searchWildlife') }}">
-                            {{-- uhm di pani sure ?? --}}
-                            <div class="input-group">
-                                <input type="search" name="searchWildlife" class="form-control mr-sm2"
-                                    placeholder="Search Critter Name">
-                                <div class="input-group-btn">
-                                    <div class="btn-group" role="group">
-                                        <div class="dropdown dropdown-lg">
-                                            <button type="button" class="btn btn-outline-dark" data-bs-toggle="modal"
-                                                data-bs-target="#ModalSearch"><i class='bx bx-filter-alt'></i></button>
-                                        </div>
-                                        <button class="btn btn-info " type="submit">Search</button>
-                                    </div>
+            <div class="col-12 col-md-6 col-lg-6 order-sm-1 order-md-2">
+                <form style="text-align: center;"class="form-inline my-2 my-lg=0" type="get"
+                    action="{{ route('S_searchWildlife') }}">
+                    <div class="input-group">
+                        <input type="search" name="searchWildlife" class="form-control mr-sm2"
+                            placeholder="Search Critter Name">
+                        <div class="input-group-btn">
+                            <div class="btn-group" role="group">
+                                <div class="dropdown dropdown-lg">
+                                    <button type="button" class="btn btn-outline-dark" data-bs-toggle="modal"
+                                        data-bs-target="#ModalSearch"><i class='bx bx-filter-alt'></i></button>
+                                </div>
+                                <button class="btn btn-info " type="submit">Search</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    {{-- Information Cards --}}
+    <section class="col-12">
+        <div class="container-fluid">
+            <div class="row g-5 m-4 p-0 d-flex align-items-stretch">
+                @foreach ($studentDashboard as $item)
+                    <div class="col-12 col-md-4 col-lg-3 d-flex align-items-stretch">
+                        <div class="card" style="width: 18rem;" data-bs-toggle="modal"
+                            data-bs-target="#ModalWildlife{{ $item->info_ID }}">
+                            <img class="card-img-top "src="{{ asset($item->wildlife_pic) }}" alt="Card image cap">
+                            <div class="card-body bg-light text-primary">
+                                <h5 class="card-title text-center fst-italic">
+                                    {{ $item->wildlife_scientific_name }}
+                                </h5>
+                                <p class="card-text text-center">{{ $item->wildlife_name }}</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    @include('IEMS.Linus.STUDENT.displayWildlife')
+                @endforeach
+            </div>
+            <!--end of catalog-->
+            <!-- Add Button -->
+
+        </div>
+    </section>
+    <!--advance search-->
+    <form action="{{ route('S_advanceSearchWildlife') }}" method="GET" enctype="multipart/form-data">
+        {!! csrf_field() !!}
+        <div class="modal fade" id="ModalSearch" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg">
+                <div class="modal-content  bg-light">
+
+                    <div class="modal-header border-0 text-center">
+                        <h5 class="modal-title  text-center">Advance Search</h5>
+                        <button type="button" class="btn-close btn-info bg-info" data-bs-dismiss="modal"
+                            aria-label="Close"></button>
+                    </div>
+
+                    <div class="modal-body">
+                        <div class="container-fluid">
+                            <div class="row g-4 m-4 p-0 d-flex align-items-stretch g-l">
+
+                                <div class="col-12">
+                                    <label class="focus-label">Critter Class:</label>
+                                    <select class="select floating" id="" name="wildlife_class">
+                                        <option></option>
+                                        @foreach ($searchClass as $item)
+                                            <option value="{{ $item->wildlife_class }}">{{ $item->wildlife_class }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+
+                                <div class="col-12">
+                                    <label class="focus-label">Critter Specie:</label>
+                                    <select class="select floating" id="" name="wildlife_species">
+                                        <option></option>
+                                        @foreach ($searchSpecie as $item)
+                                            <option value="{{ $item->wildlife_species }}">{{ $item->wildlife_species }}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+
+                                <div class="col-12">
+                                    <label class="focus-label">Wildlife Location:</label>
+                                    <select class="select floating" id="" name="wildlife_location">
+                                        <option></option>
+                                        @foreach ($searchLoc as $item)
+                                            <option value="{{ $item->wildlife_location }}">{{ $item->wildlife_location }}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+
+                                <div class="modal-footer border-0">
+                                    <button type="submit" class="btn btn-info text-white">Search</button>
+                                    <button type="button" class="btn btn-outline-info"
+                                        data-bs-dismiss="modal">Cancel</button>
                                 </div>
                             </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
-
-
-
-
-        {{-- Information Cards --}}
-        <section class="col-12">
-            <div class="container-fluid">
-                <div class="row g-5 m-4 p-0 d-flex align-items-stretch">
-                    @foreach ($studentDashboard as $item)
-                        <div class="col-12 col-md-4 col-lg-3 d-flex align-items-stretch">
-                            <div class="card" style="width: 18rem;" data-bs-toggle="modal"
-                                data-bs-target="#ModalWildlife{{ $item->info_ID }}">
-                                <img class="card-img-top "src="{{ asset($item->wildlife_pic) }}" alt="Card image cap">
-                                <div class="card-body bg-light text-primary">
-                                    <h5 class="card-title text-center fst-italic">
-                                        {{ $item->wildlife_scientific_name }}
-                                    </h5>
-                                    <p class="card-text text-center">{{ $item->wildlife_name }}</p>
-                                </div>
-                            </div>
-
-                        </div>
-                        @include('IEMS.Linus.STUDENT.displayWildlife')
-                    @endforeach
-                </div>
-                <!--end of catalog-->
-                <!-- Add Button -->
-
-            </div>
-        </section>
-    <!--advance search-->
-<form action="{{ route('S_advanceSearchWildlife') }}" method="GET" enctype="multipart/form-data">
-      {!! csrf_field() !!}
-      <div class="modal fade" id="ModalSearch" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg">
-          <div class="modal-content  bg-light">
-
-            <div class="modal-header border-0 text-center">
-              <h5 class="modal-title  text-center">Advance Search</h5>
-              <button type="button" class="btn-close btn-info bg-info" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-
-            <div class="modal-body">
-              <div class="container-fluid">
-                <div class="row g-4 m-4 p-0 d-flex align-items-stretch g-l">
-
-                  <div class="col-12">
-                  <label class="focus-label">Critter Class:</label>
-                    <select class="select floating" id="" name="wildlife_class">
-                      <option></option>
-                      @foreach($searchClass as $item)
-                      <option value="{{ $item->wildlife_class }}">{{$item->wildlife_class}}</option>
-                      @endforeach
-                    </select>
-                  </div>
-
-                  <div class="col-12">
-                  <label class="focus-label">Critter Specie:</label>
-                    <select class="select floating" id="" name="wildlife_species">
-                      <option></option>
-                      @foreach($searchSpecie as $item)
-                      <option value="{{ $item->wildlife_species }}">{{$item->wildlife_species}}</option>
-                      @endforeach
-                    </select>
-                  </div>
-
-                  <div class="col-12">
-                  <label class="focus-label">Wildlife Location:</label>
-                    <select class="select floating" id="" name="wildlife_location">
-                      <option></option>
-                      @foreach($searchLoc as $item)
-                      <option value="{{ $item->wildlife_location }}">{{$item->wildlife_location}}</option>
-                      @endforeach
-                    </select>
-                  </div>
-
-                  <div class="modal-footer border-0">
-                    <button type="submit" class="btn btn-info text-white">Search</button>
-                    <button type="button" class="btn btn-outline-info" data-bs-dismiss="modal">Cancel</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-</form><!--end of form-->
+    </form>
+    <!--end of form-->
     </div>
 @endsection
