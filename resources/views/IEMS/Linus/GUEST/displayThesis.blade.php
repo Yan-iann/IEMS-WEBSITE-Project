f3d8c5a9c82b9309c6998a0dd854bed40a590e51
Jibrael Yap <jibraelpyap@su.edu.ph>
2022-11-27 15:35:38 +0800

    Modal for thesis and journal view message
     Changes to be committed:
    	modified:   resources/views/IEMS/Linus/GUEST/displayJournal.blade.php
    	modified:   resources/views/IEMS/Linus/GUEST/displayThesis.blade.php
    	modified:   resources/views/IEMS/Linus/STUDENT/displayJournal.blade.php
    	modified:   resources/views/IEMS/Linus/STUDENT/displayThesis.blade.php

--- resources/views/IEMS/Linus/GUEST/displayThesis.blade.php
+++ resources/views/IEMS/Linus/GUEST/displayThesis.blade.php
@@ -1,74 +1,79 @@
 {{-- Infocard Modal --}}
-<div class="modal fade" id="ModalThesis{{$item->info_ID}}" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg">
-    <div class="modal-content bg-light">
+<div class="modal fade" id="ModalThesis{{ $item->info_ID }}" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg">
+        <div class="modal-content bg-light">
 
-      <div class="modal-header border-0 text-center">
-        <h5 class="modal-title  text-center">Thesis Paper Details</h5>
-        <button type="button" class="btn-close btn-info bg-info" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-
-      <div class="modal-body">
-        <div class="container-fluid">
-          <div class="row g-4 m-4 p-0 d-flex align-items-stretch g-l">
-
-
-            <div class="col-12">
-              <label for="formGroupExampleInput" class="form-label">Thesis Title</label>
-              <h2 class="detailsView">{{ $item->thesis_title}}</h2 >
+            <div class="modal-header border-0 text-center">
+                <h5 class="modal-title  text-center">Thesis Paper Details</h5>
+                <button type="button" class="btn-close btn-info bg-info" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
             </div>
 
-            <div class="col-12">
-                <label for="formGroupExampleInput" class="form-label">Author/s</label>
-                <p >{{ $item->thesis_author}}</p>
+            <div class="modal-body">
+                <div class="container-fluid">
+                    <div class="row g-4 m-4 p-0 d-flex align-items-stretch g-l">
+
+
+                        <div class="col-12">
+                            <label for="formGroupExampleInput" class="form-label">Thesis Title</label>
+                            <h2 class="detailsView">{{ $item->thesis_title }}</h2>
+                        </div>
+
+                        <div class="col-12">
+                            <label for="formGroupExampleInput" class="form-label">Author/s</label>
+                            <p>{{ $item->thesis_author }}</p>
+                        </div>
+
+                        <div class="col-12">
+                            <label for="formGroupExampleInput2" class="form-label">Thesis Reference</label>
+                            <p>{{ $item->thesis_reference }}</p>
+                        </div>
+
+                        <div class="col-12">
+                            <!-- filler -->
+                        </div>
+
+                        <div class="col-12">
+                            <p class="card-text text-muted fst-italic">{{ $item->date_published }}</p>
+                        </div>
+
+
+
+                    </div>
+                </div>
             </div>
-
-            <div class="col-12">
-              <label for="formGroupExampleInput2" class="form-label">Thesis Reference</label>
-              <p>{{ $item->thesis_reference}}</p>
+            <div class="modal-footer border-0">
+                <button type="button" class="btn btn-info" data-bs-toggle="modal"
+                    data-bs-target="#Notify{{ $item->info_ID }}">View</button>
             </div>
-
-            <div class="col-12">
-              <!-- filler -->
-            </div>
-
-            <div class="col-12">
-              <p class="card-text text-muted fst-italic">{{ $item->date_published}}</p>
-            </div>
-
-
-
-          </div>
         </div>
-      </div>
-      <div class="modal-footer border-0">
-        <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#Notify{{$item->info_ID}}">View</button>
-      </div>
     </div>
-  </div>
 </div>
 
 
 {{-- Notify Modal --}}
-<div class="modal fade" id="Notify{{$item->info_ID}}" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+<div class="modal fade" id="Notify{{ $item->info_ID }}" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable ">
-      <div class="modal-content bg-light">
+        <div class="modal-content bg-light">
 
-        <div class="modal-header border-0 text-center">
-          <h5 class="modal-title  text-center"></h5>
-          <button type="button" class="btn-close btn-info bg-info" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-
-        <div class="modal-body border-0">
-          <div class="container-fluid">
-            <div class="row g-4 m-4 p-0 d-flex align-items-stretch g-l">
-              <p> To view this document, please email us at <a href="mailto:iems@su.edu.ph">iems@su.edu.ph </a> regarding on viewing this document</p>
+            <div class="modal-header border-0 text-center">
+                <h5 class="modal-title  text-center"></h5>
+                <button type="button" class="btn-close btn-info bg-info" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
             </div>
-          </div>
+
+            <div class="modal-body border-0">
+                <div class="container-fluid">
+                    <div class="row g-4 m-4 p-0 d-flex align-items-stretch g-l">
+                        <p> Please email us at <a href="mailto:iems@su.edu.ph">iems@su.edu.ph </a> to view this document
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer border-0">
+                <button type="button" class="btn btn-info" data-bs-toggle="modal"
+                    data-bs-target="#ModalThesis{{ $item->info_ID }}">Okay</button>
+            </div>
         </div>
-        <div class="modal-footer border-0">
-          <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#ModalThesis{{$item->info_ID}}">Okay</button>
-        </div>
-      </div>
     </div>
-  </div>
+</div>
